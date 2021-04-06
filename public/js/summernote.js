f54b8d3734fe6f4620d27a6d232479c23fcfc193
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-04-06 20:56:45 +0530

    Add toolbar options to editor

--- public/js/summernote.js
+++ public/js/summernote.js
@@ -5,5 +5,17 @@ $(document).ready(function() {
         maxHeight: null,
         spellCheck: true,
         focus: true,
+        toolbar: [
+            // [groupName, [list of button]]
+            ['basic', ['style', 'fontname', 'fontsize']],
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['forecolor', 'backcolor']],
+            // ['block', ['ul', 'ol', 'paragraph']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['media', ['link', 'picture', 'video', 'table', 'hr']],
+            ['height', ['height', 'codeview', 'fullscreen', 'undo', 'redo']]
+          ]
     })
 });
